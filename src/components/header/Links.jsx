6894e9a0e8a2b955ba032ac2f242e7cb72e1b31e
Alejandro <alejandrom.am33@gmail.com>
2022-08-26 22:40:03 -0500

    update: ig link

--- src/components/header/Links.jsx
+++ src/components/header/Links.jsx
@@ -7,7 +7,7 @@ export default function Links() {
             <a href="https://github.com/AlejandroMorales-s" target='_blank'>
                 <AiOutlineGithub className='header-icon'/>
             </a>
-            <a href="https://www.instagram.com/alejandrom0_0/" target='_blank'>
+            <a href="https://www.instagram.com/alejandro_m_e/" target='_blank'>
                 <AiOutlineInstagram className='header-icon'/>
             </a>
             <a href="https://www.linkedin.com/in/alejandro-morales-0b7513200/" target='_blank'>

1fc16ab41d56aa7192b121388d4ebdf3149df6bb
Alejandro <alejandrom.am33@gmail.com>
2022-08-27 15:37:57 -0500

    change: refactor in skills component code

--- src/components/home/skills/Skills.jsx
+++ src/components/home/skills/Skills.jsx
@@ -4,42 +4,52 @@ import {SiTailwindcss, SiJavascript} from 'react-icons/si';
 import {IoLogoCss3, IoLogoNodejs} from 'react-icons/io';
 
 export default function Skills() {
+
+    const skills = [
+        {
+            name: 'React',
+            icon: <FaReact className='skill-icon react'/>,
+        },
+        {
+            name: 'Sass',
+            icon: <FaSass className='skill-icon sass'/>,
+        },
+        {
+            name: 'HTML 5',
+            icon: <FaHtml5 className='skill-icon html'/>,
+        },
+        {
+            name: 'Tailwind',
+            icon: <SiTailwindcss className='skill-icon tailwind'/>,
+        },
+        {
+            name: 'JavaScript',
+            icon: <SiJavascript className='skill-icon js'/>,
+        },
+        {
+            name: 'CSS 3',
+            icon: <IoLogoCss3 className='skill-icon css'/>,
+        },
+        {
+            name: 'Git',
+            icon: <FaGitAlt className='skill-icon git'/>,
+        },
+        {
+            name: 'Node JS',
+            icon: <IoLogoNodejs className='skill-icon node'/>,
+        }
+    ];
+
     return (
         <div id='skills' className='container skills'>
             <h2 className='h2'>Skills</h2>
             <div className='skills-container'>
-                <div className='skill'>
-                    <FaReact className='react skill-icon'/>
-                    <p>React</p>
-                </div>
-                <div className='skill'>
-                    <FaSass className='sass skill-icon'/>
-                    <p>Sass</p>
-                </div>
-                <div className='skill'>
-                    <FaHtml5 className='html skill-icon'/>
-                    <p>HTML 5</p>
-                </div>
-                <div className='skill'>
-                    <SiTailwindcss className='tailwind skill-icon'/>
-                    <p>Tailwind</p>
-                </div>
-                <div className='skill'>
-                    <SiJavascript className='skill-icon js'/>
-                    <p>JavaScript</p>
-                </div>
-                <div className='skill'>
-                    <IoLogoCss3 className='skill-icon css'/>
-                    <p>CSS</p>
-                </div>
-                <div className='skill'>
-                    <FaGitAlt className='skill-icon git'/>
-                    <p>Git</p>
-                </div>
-                <div className='skill'>
-                    <IoLogoNodejs className='skill-icon node'/>
-                    <p>Node JS</p>
-                </div>
+                {skills.map(skill => {
+                    return <div key={skill.name} className='skill'>
+                        {skill.icon} 
+                        <p>{skill.name}</p>
+                    </div>
+                })}
             </div>
         </div>
     )

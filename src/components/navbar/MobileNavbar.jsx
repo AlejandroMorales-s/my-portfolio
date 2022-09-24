02c22665660a849b4c6eb09e1f39a0167b9c8e72
Alejandro <alejandrom.am33@gmail.com>
2022-09-24 11:09:35 -0500

    add: navbar for mobile

--- src/components/navbar/MobileNavbar.jsx
+++ src/components/navbar/MobileNavbar.jsx
@@ -1,9 +1,36 @@
+import { useState } from 'react'
 import {MdMenu} from 'react-icons/md'
+import {AiOutlineDownload} from 'react-icons/ai';
 
 export default function MobileNavbar() {
+    const [showNav, setShowNav] = useState(false)
+
+    //* Smooth scroll
+    const scroll = (e) => {
+        e.preventDefault();
+        const href = e.target.attributes.href.value;
+        const section =  document.querySelector(href);
+        section.scrollIntoView({behavior: "smooth"});
+        setShowNav(false)
+    };
+    
     return (
         <div className='mobile-menu'>
-            <MdMenu className='mobile-menu-icon'/>
+            <MdMenu onClick={() => setShowNav(!showNav)} className={`mobile-menu-icon ${showNav ? 'icon-active' : 'icon-inactive'}`}/>
+            <nav className={`mobile-nav ${showNav ? 'show-item' : 'hide-item'}`}>
+                    <ul>
+                        <li><a onClick={(e) => scroll(e)} className='nav-button' href='#skills'>Skills</a></li>
+                        <li><a onClick={(e) => scroll(e)} className='nav-button' href='#about'>About</a></li>
+                        <li><a onClick={(e) => scroll(e)} className='nav-button' href='#projects'>Projects</a></li>
+                        <li><a onClick={(e) => scroll(e)} className='nav-button' href='#certificates'>Certificates</a></li>
+                        <li>
+                            <button>
+                                CV
+                                <AiOutlineDownload className='download-icon'/>
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
         </div>
     )
 }

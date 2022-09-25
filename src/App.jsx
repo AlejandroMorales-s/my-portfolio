883605a54e97e016df1c271fcb18df20c6d04dbb
Alejandro <alejandrom.am33@gmail.com>
2022-09-25 18:51:01 -0500

    add: return to top button and when changing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
 import './App.scss';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, useLocation} from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/navbar/Navbar';import Certificates from './pages/Certificates';
-;
+import Projects from './pages/Projects';
+import { useState } from 'react';
+import {FaArrowCircleUp} from 'react-icons/fa'
+import { useEffect } from 'react';
 
 function App() {
+  let location = useLocation()
+
+  const [showBtn, setShowBtn] = useState(false)
+  
+  window.addEventListener('scroll', () => window.scrollY >= window.innerHeight / 2 ? setShowBtn(true) : setShowBtn(false));
+
+  const scroll = () => {
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth"
+    });
+  };
+
+  useEffect(() => {
+    scroll() 
+  }, [location])
+  
   return (
     <>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/certificates" element={<Certificates/>} />
+        <Route path="/projects" element={<Projects/>} />
       </Routes>
+      {showBtn && 
+        <FaArrowCircleUp onClick={scroll} className='go-to-top-button' />
+      }
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,30 +4,30 @@ import {Link} from 'react-router-dom';
 import MobileNavbar from './MobileNavbar';
 
 export default function Navbar() {
-    const [showNavbar, setShowNavbar] = useState(true);
+    const [showNavbar, setShowNavbar] = useState(true)
 
     //* Show/Hide Navbar on scroll
-    let currentPosition  = window.pageYOffset;
+    let currentPosition  = window.pageYOffset
 
     window.onscroll = () => {
-        let scrolling = window.pageYOffset;
-        currentPosition >= scrolling ? setShowNavbar(true) : setShowNavbar(false);
-        currentPosition = scrolling;
-    };
+        let scrolling = window.pageYOffset
+        currentPosition >= scrolling ? setShowNavbar(true) : setShowNavbar(false)
+        currentPosition = scrolling
+    }
 
     //* Smooth scroll
     const scroll = (e) => {
-        e.preventDefault();
-        const href = e.target.attributes.href.value;
-        const section =  document.querySelector(href);
-        section.scrollIntoView({behavior: "smooth"});
-    };
+        e.preventDefault()
+        const href = e.target.attributes.href.value
+        const section =  document.querySelector(href)
+        section.scrollIntoView({behavior: "smooth"})
+    }
 
     return (
         <div className={`${showNavbar ? 'show-navbar' : 'hide-navbar'} navbar-container`}>
             <div className='container navbar'>
                 <Link to='/'>
-                    <h1>Alejandro Morales</h1>
+                    <h1 className='logo'>Alejandro Morales</h1>
                 </Link>
                 <nav className='desktop-menu'>
                     <ul>

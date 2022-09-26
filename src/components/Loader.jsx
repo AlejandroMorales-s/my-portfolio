94b1c194a9b968b6f239f790136d3c7ed5398742
Alejandro <alejandrom.am33@gmail.com>
2022-09-25 22:29:10 -0500

    fix: class to className

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Loader() {
     return (
         <div className='loader-container'>
-            <div class="leap-frog">
-                <div class="leap-frog__dot"></div>
-                <div class="leap-frog__dot"></div>
-                <div class="leap-frog__dot"></div>
+            <div className="leap-frog">
+                <div className="leap-frog__dot"></div>
+                <div className="leap-frog__dot"></div>
+                <div className="leap-frog__dot"></div>
             </div>
         </div>
     )

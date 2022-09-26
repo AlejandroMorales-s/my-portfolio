4e1a50d599431aa074fe132d1cff13c74f1ce897
Alejandro <alejandrom.am33@gmail.com>
2022-09-25 20:45:37 -0500

    add: loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Loader() {
+    return (
+        <div className='loader-container'>
+            <div class="leap-frog">
+                <div class="leap-frog__dot"></div>
+                <div class="leap-frog__dot"></div>
+                <div class="leap-frog__dot"></div>
+            </div>
+        </div>
+    )
+}

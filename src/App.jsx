f27c1d2f3148172f1c30f404b167e5f106145bbe
Alejandro <alejandrom.am33@gmail.com>
2023-03-03 00:02:04 -0600

    remove project details component

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,48 @@
-import './App.scss';
-import {Routes, Route, useLocation} from 'react-router-dom';
-import Home from './pages/Home';
-import Navbar from './components/navbar/Navbar';import Certificates from './pages/Certificates';
-import Projects from './pages/Projects';
-import { useState } from 'react';
-import {FaArrowCircleUp} from 'react-icons/fa'
-import { useEffect } from 'react';
-import ProjectDetails from './pages/ProjectDetails';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/Home";
+import Navbar from "./components/navbar/Navbar";
+import Certificates from "./pages/Certificates";
+import Projects from "./pages/Projects";
+import { FaArrowCircleUp } from "react-icons/fa";
 
 function App() {
-  let location = useLocation()
+  const location = useLocation();
 
-  const [showBtn, setShowBtn] = useState(false)
-  
-  window.addEventListener('scroll', () => window.scrollY >= window.innerHeight / 2 ? setShowBtn(true) : setShowBtn(false));
+  const [showBtn, setShowBtn] = useState(false);
+
+  window.addEventListener("scroll", () =>
+    window.scrollY >= window.innerHeight / 2
+      ? setShowBtn(true)
+      : setShowBtn(false)
+  );
 
   const scroll = () => {
     window.scrollTo({
-        top:0,
-        left:0,
-        behavior:"smooth"
+      top: 0,
+      left: 0,
+      behavior: "smooth",
     });
   };
 
   useEffect(() => {
-    scroll() 
-  }, [location])
-  
+    scroll();
+  }, [location]);
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/certificates" element={<Certificates/>} />
-        <Route path="/projects" element={<Projects/>} />
-        <Route path="/project-details/:id" element={<ProjectDetails/>} />
+        <Route path="/certificates" element={<Certificates />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
-      {showBtn && 
-        <FaArrowCircleUp onClick={scroll} className='go-to-top-button' />
-      }
+      {showBtn && (
+        <FaArrowCircleUp onClick={scroll} className="go-to-top-button" />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

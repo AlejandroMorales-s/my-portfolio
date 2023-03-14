0a894d4486181923f66c37b7fd6d574a7b2218c3
Alejandro  Morales <77386102+AlejandroMorales-s@users.noreply.github.com>
2023-03-14 04:03:07 -0600

    fixed lang of the about section

--- src/components/home/about/About.jsx
+++ src/components/home/about/About.jsx
@@ -1,37 +1,36 @@
-import React from "react";
-import ProfilePhoto from "../../ProfilePhoto";
-import { saveAs } from "file-saver";
+import React from 'react';
+import ProfilePhoto from '../../ProfilePhoto';
+import { saveAs } from 'file-saver';
 
 export default function About() {
   const downloadPDF = () => {
-    fetch("./CV.pdf")
+    fetch('./CV.pdf')
       .then((response) => response.blob())
       .then((blob) => {
-        saveAs(blob, "cv.pdf");
+        saveAs(blob, 'cv.pdf');
       })
       .catch((error) => {
-        console.error("Error:", error);
+        console.error('Error:', error);
       });
   };
 
   return (
-    <div id="about" className="container about">
-      <h2 className="h2">About</h2>
-      <div className="about-container">
+    <div id='about' className='container about'>
+      <h2 className='h2'>About</h2>
+      <div className='about-container'>
         <ProfilePhoto />
         <div>
           <p>
-            Soy un desarrollador de front-end con una gran pasión por el
-            desarrollo de aplicaciones web interactivas y dinámicas. Me
-            especializo en React y tengo experiencia desarrollando varios
-            proyectos personales. Además, he trabajado en proyectos de equipo
-            donde he aprendido mucho y he aportado mis habilidades técnicas para
-            lograr un resultado de alta calidad. Me gusta mantenerme actualizado
-            en las últimas tendencias y tecnologías en el campo del desarrollo
-            de aplicaciones web para asegurarme de hacer mi trabajo lo mejor
-            posible.
+            I am a front-end developer with a great passion for developing
+            interactive and dynamic web applications. I specialize in React and
+            have experience developing several personal projects. In addition, I
+            have worked on team projects where I have learned a lot and
+            contributed my technical skills to achieve a high quality result. I
+            like to keep myself updated on the latest trends and technologies in
+            the field of web application development to make sure I do my job to
+            the best of my ability.
           </p>
-          <button onClick={downloadPDF} type="button">
+          <button onClick={downloadPDF} type='button'>
             Download CV
           </button>
         </div>

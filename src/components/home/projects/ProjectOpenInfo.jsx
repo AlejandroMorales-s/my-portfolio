de9a8e5f96ffee46c7ec978978c587085d91e7c2
Alejandro <alejandrom.am33@gmail.com>
2023-03-19 23:17:39 -0600

    fixed projects info component design

--- src/components/home/projects/ProjectOpenInfo.jsx
+++ src/components/home/projects/ProjectOpenInfo.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
-import { AiOutlineLeft, AiOutlineRight, AiOutlineClose } from "react-icons/ai";
+import {
+  AiOutlineClose,
+  AiOutlineMobile,
+  AiOutlineDesktop,
+} from "react-icons/ai";
+import { BsTablet, BsLaptop } from "react-icons/bs";
 import returnTechIcon from "../../../utils/returnTechIcon";
 
-export default function ProjectOpenInfo({
-  projectInfo,
-  openProjectInfo,
-  setOpenProjectInfo,
-}) {
+export default function ProjectOpenInfo({ projectInfo, setOpenProjectInfo }) {
+  const {
+    name,
+    images,
+    info,
+    functions,
+    repoLink,
+    siteLink,
+    devices,
+    technologies,
+  } = projectInfo;
+
+  const devicesIcons = {
+    mobile: <AiOutlineMobile className="device-supported-icon" />,
+    tablet: <BsTablet className="device-supported-icon" />,
+    laptop: <BsLaptop className="device-supported-icon" />,
+    desktop: <AiOutlineDesktop className="device-supported-icon" />,
+  };
+
   const [closingPopup, setClosingPopup] = useState(false);
-  const { name, images, info, functions, repoLink, siteLink, technologies } =
-    projectInfo;
 
   return (
     <div
@@ -35,51 +52,61 @@ export default function ProjectOpenInfo({
         </div>
         <h2 className="h2">{name}</h2>
         <div className="project-images-container">
-          {/* <div className="project-images-icons-container">
-          <AiOutlineLeft
-            onClick={() => previousImage()}
-            className="project-images-icon"
-          />
-        </div> */}
           <div className="project-image-container">
-            <img
-              // src={ProfilePhoto}
-              srcSet={images[0]}
-              alt={name}
-            />
+            <img srcSet={images[0]} alt={name} />
           </div>
-          {/* <div className="project-images-icons-container">
-          <AiOutlineRight
-            // onClick={() => nextImage()}
-            className="project-images-icon"
-          />
-        </div> */}
         </div>
         <div className="info-container">
-          <div className="project-info-technologies">
-            {technologies.map((technology) => (
-              <div
-                key={technology}
-                className={`${`${technology.toLowerCase()}-project-card`} project-card-technology`}
-              >
-                <p>{technology}</p>
-                {returnTechIcon(technology)}
-              </div>
-            ))}
+          <div className="devices-supported">
+            <h3>Devices supported</h3>
+            <div className="devices-supported-container">
+              {devices.map((device) => {
+                const deviceCapitalized =
+                  device.charAt(0).toUpperCase() + device.slice(1);
+
+                return (
+                  <div className="device-supported-container" key={device}>
+                    <p>{deviceCapitalized}</p>
+                    {devicesIcons[device]}
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="info">
+          <div className="project-info-technologies">
+            <h3>Tech stack</h3>
+            <div className="project-info-technologies-container">
+              {technologies.map((technology) => (
+                <div
+                  key={technology}
+                  className={`${`${technology.toLowerCase()}-project-card`} project-card-technology`}
+                >
+                  <p
+                    className={`${technology.toLowerCase()} project-card-tech-name`}
+                  >
+                    {technology}
+                  </p>
+                  {returnTechIcon(technology)}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="description-container">
+            <h3>Description</h3>
             <p>{info}</p>
           </div>
-          {functions && (
-            <>
-              <h3>Funcionalidades disponibles:</h3>
-              <ul className="functionalities-list">
-                {functions.map((element) => (
-                  <li key={element}>{element}</li>
-                ))}
-              </ul>
-            </>
-          )}
+          <div className="functionalities-container">
+            {functions && (
+              <>
+                <h3>Functionalities</h3>
+                <ul className="functionalities-list">
+                  {functions.map((element) => (
+                    <li key={element}>{element}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </div>
           <div className="project-buttons-container">
             <a href={repoLink} target="_blank" rel="noreferrer">
               <button type="button">Repositorio</button>

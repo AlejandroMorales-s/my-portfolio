3a6d171ad223f833296ea2638caee5fe38a1c9fd
Alejandro <alejandrom.am33@gmail.com>
2023-03-19 21:37:53 -0600

    changed job title

--- src/components/home/header/ProfileInfo.jsx
+++ src/components/home/header/ProfileInfo.jsx
@@ -6,7 +6,7 @@ export default function ProfileInfo() {
     <div className="header-info-container">
       <h2>Alejandro Morales</h2>
       <ProfilePhoto />
-      <p className="job-title">Front-End Developer</p>
+      <h2 className="job-title">Front-End Developer</h2>
     </div>
   );
 }

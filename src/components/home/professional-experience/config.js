7932df9bdfa3fd93ab1300874c86402af1cdc6d6
Alejandro Morales <alejandromorales@MacBook-Pro-de-Alejandro.local>
2024-01-21 19:10:29 -0600

    feat(home): professional experience section

---
+++ src/components/home/professional-experience/config.js
@@ -0,0 +1,22 @@
+export const jobsList = [
+  {
+    title: "Junior Frontend Developer",
+    company: "Cicada Technologies Inc.",
+    companyLink: "https://www.cicada.io",
+    location: "Mexico City, Mexico",
+    date: "Dec 2023 - Present",
+    description: [],
+    modality: "Hybrid",
+    technologies: [],
+  },
+  {
+    title: "Frontend Developer Intern",
+    company: "Cicada Technologies Inc.",
+    companyLink: "https://www.cicada.io",
+    location: "Mexico City, Mexico",
+    date: "Jun 2023 - Dec 2023",
+    description: [],
+    modality: "Highly remote",
+    technologies: [],
+  },
+];
